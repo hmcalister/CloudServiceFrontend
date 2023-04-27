1e88a26c5ae33b05f7e558c0c2efea996c2c9893
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-27 23:44:22 +1200

    Update App.svelte to add project code

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { loggedIn as loggedInStore } from "./lib/store";
+    import LoginModal from "./lib/LoginModal.svelte";
+    import LogoutButton from "./lib/LogoutButton.svelte";
+
+    let loggedInBool;
+    loggedInStore.subscribe((loggedIn) => {
+        loggedInBool = loggedIn;
+    });
+</script>
+
+<main class="grid place-items-center w-screen h-screen">
+    <!-- Login Modal if not Logged In -->
+    {#if !loggedInBool}
+        <LoginModal />
+    {:else}
+        <div>
+            <LogoutButton />
+        </div>
+    {/if}
+</main>
+
+<style global lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
