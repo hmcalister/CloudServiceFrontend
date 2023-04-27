19018e82f0d41df4f702bc148b35651f73e9855e
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-27 23:44:36 +1200

    Add login and logout primitive implementations

---
+++ src/lib/LoginModal.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { loggedIn } from "./store.js";
+
+    function LogIn() {
+        loggedIn.update((_) => true);
+    }
+</script>
+
+<label for="LogIn-Modal" class="btn">Log In</label>
+
+<input type="checkbox" id="LogIn-Modal" class="modal-toggle" />
+<label for="LogIn-Modal" class="modal cursor-pointer">
+    <label class="modal-box relative">
+        <h3>Log In</h3>
+        <p class="py-4">Some Login Text</p>
+        <div class="modal-action">
+            <button class="btn" on:click={LogIn}>Log In</button>
+        </div>
+    </label>
+</label>
---
+++ src/lib/LogoutButton.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { loggedIn } from "./store";
+
+    function LogOut() {
+        loggedIn.update(_ => false);
+    }
+</script>
+
+<button class="btn" on:click={LogOut}>Log Out</button>
